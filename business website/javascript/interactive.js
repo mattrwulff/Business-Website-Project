d8cb4565de87e13e3a6c5db362b035b85b2c6c7e
Matt <mattrwulff@gmail.com>
2020-03-13 15:24:25 -0700

    worked on content

--- business website/javascript/interactive.js
+++ business website/javascript/interactive.js
@@ -3,7 +3,9 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("navbar").style.top = "0";
+    document.getElementById("nav").style.visibility="hidden";
   } else {
     document.getElementById("navbar").style.top = "-50px";
+    document.getElementById("nav").style.visibility="visible";
   }
 }
